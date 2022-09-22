29222af0f56a4991aa4f07773fae8011fdfcaf04
brad-bb-bell <brad.bell5@montana.edu>
2022-09-22 11:12:46 -0600

    gets grading-students.js started

--- HackerRank/grading-students.js
+++ HackerRank/grading-students.js
@@ -1,1 +1,16 @@
-// Test
+function gradingStudents(grades) {
+  grades.forEach(grade => {
+    if (grade % 5 !== 0) {
+        console.log("not divisible");
+    } else if (grade % 5 === 0) {
+        console.log("divisible")
+    }
+    //   var roundUp = grade;
+    //   for (let index = 1; grade % 5 === 0; index++) {
+    //     roundUp = grade + index;
+    //     console.log(roundUp);   
+    //   }
+s
+  });
+}
+console.log(gradingStudents([73,67,38,33]));
