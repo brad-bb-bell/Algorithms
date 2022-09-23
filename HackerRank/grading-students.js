c7aded955ad7dda8d0f94871cbcc0178ce6f4457
brad-bb-bell <brad.bell5@montana.edu>
2022-09-23 09:39:26 -0600

    updates grading-students.js to pass all edge cases

--- HackerRank/grading-students.js
+++ HackerRank/grading-students.js
@@ -1,7 +1,7 @@
 function gradingStudents(grades) {
   var solution = [];
   grades.forEach((grade) => {
-    if (grade % 5 !== 0 && grade > 33) {
+    if (grade % 5 !== 0 && grade > 34) {
       for (let roundUp = grade; roundUp % 5 !== 0; roundUp++) {
         if ((roundUp + 1) % 5 === 0) {
           if (roundUp + 1 - grade < 3) {
@@ -18,6 +18,4 @@ function gradingStudents(grades) {
   return solution;
 }
 console.log(gradingStudents([75, 73, 67, 38, 33]));
-console.log(
-  gradingStudents([22, 86, 30, 0, 16, 51, 53, 42, 48, 22, 69, 12, 27, 34, 24, 95, 16, 32, 22, 52, 56, 71, 95])
-);
+console.log(gradingStudents([86, 30, 0, 16, 51, 53, 42, 48, 22, 69, 12, 27, 34, 24, 95, 16, 32, 22, 52, 56, 71, 95]));
