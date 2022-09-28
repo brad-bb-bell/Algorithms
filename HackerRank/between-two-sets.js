d39946212dbf77c531b86f2012819194d37d5e0e
brad-bb-bell <brad.bell5@montana.edu>
2022-09-28 12:44:04 -0600

    still working on between-two-sets

---
+++ HackerRank/between-two-sets.js
@@ -0,0 +1,23 @@
+function getTotalX(a, b) {
+  var output = 0;
+  var factors = [];
+  for (let index = 2; index <= b[0]; index++) {
+    if (b[0] % index === 0) {
+      factors.push(index);
+    }
+  }
+  factors.forEach((factor) => {
+    var istrue = [];
+    for (let index = 0; index < b.length; index++) {
+      if (b[index] % factor === 0 && factor !== 2 && factor !== 1) {
+        istrue.push(b[index]);
+      }
+    }
+    if (istrue.length === b.length) {
+      output++;
+    }
+  });
+  return output;
+}
+console.log(getTotalX([2, 4], [16, 32, 96]));
+console.log(getTotalX([3, 4], [24, 48]));
