51d030d4fb8557520694cd40701e735ef93f9aed
brad-bb-bell <brad.bell5@montana.edu>
2022-10-04 17:25:33 -0600

    still working on between-two-sets.js

--- HackerRank/between-two-sets.js
+++ HackerRank/between-two-sets.js
@@ -1,23 +1,32 @@
+// function getTotalX(a, b) {
+//   var output = 0;
+//   var factors = [];
+//   for (let index = 2; index <= b[0]; index++) {
+//     if (b[0] % index === 0) {
+//       factors.push(index);
+//     }
+//   }
+//   factors.forEach((factor) => {
+//     var istrue = [];
+//     for (let index = 0; index < b.length; index++) {
+//       if (b[index] % factor === 0 && factor !== 2 && factor !== 1) {
+//         istrue.push(b[index]);
+//       }
+//     }
+//     if (istrue.length === b.length) {
+//       output++;
+//     }
+//   });
+//   return output;
+// }
 function getTotalX(a, b) {
   var output = 0;
-  var factors = [];
-  for (let index = 2; index <= b[0]; index++) {
-    if (b[0] % index === 0) {
-      factors.push(index);
-    }
+  for (let i = Math.min(...a); i <= Math.max(...b); i += Math.min(...a)) {
+    b.array.forEach(element => {
+      a.
+      
+    });
   }
-  factors.forEach((factor) => {
-    var istrue = [];
-    for (let index = 0; index < b.length; index++) {
-      if (b[index] % factor === 0 && factor !== 2 && factor !== 1) {
-        istrue.push(b[index]);
-      }
-    }
-    if (istrue.length === b.length) {
-      output++;
-    }
-  });
-  return output;
 }
 console.log(getTotalX([2, 4], [16, 32, 96]));
 console.log(getTotalX([3, 4], [24, 48]));
