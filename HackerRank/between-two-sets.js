89b336ea4cf0f376c4ca88adffe066cd1aa6c33b
brad-bb-bell <brad.bell5@montana.edu>
2022-10-06 10:20:18 -0600

    completes between-two-sets.js

--- HackerRank/between-two-sets.js
+++ HackerRank/between-two-sets.js
@@ -1,33 +1,14 @@
-// function getTotalX(a, b) {
-//   var output = 0;
-//   var factors = [];
-//   for (let index = 2; index <= b[0]; index++) {
-//     if (b[0] % index === 0) {
-//       factors.push(index);
-//     }
-//   }
-//   factors.forEach((factor) => {
-//     var istrue = [];
-//     for (let index = 0; index < b.length; index++) {
-//       if (b[index] % factor === 0 && factor !== 2 && factor !== 1) {
-//         istrue.push(b[index]);
-//       }
-//     }
-//     if (istrue.length === b.length) {
-//       output++;
-//     }
-//   });
-//   return output;
-// }
 function getTotalX(a, b) {
   var output = 0;
-  for (let i = Math.min(...a); i <= Math.max(...b); i += Math.min(...a)) {
-    b.array.forEach(element => {
-      a.forEach((x => 
-        ))
-      
-    });
+  var min_a = Math.min(...a);
+  for (let i = min_a; i <= Math.max(...b); i += min_a) {
+    if (b.every((item) => item % i === 0)) {
+      if (a.every((item) => i % item === 0)) {
+        output++;
+      }
+    }
   }
+  return output;
 }
 console.log(getTotalX([2, 4], [16, 32, 96]));
 console.log(getTotalX([3, 4], [24, 48]));
