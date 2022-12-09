6c0b8f6f76e75d039d88cb7094fbf935fe8269d2
brad-bb-bell <brad.bell5@montana.edu>
2022-12-09 16:07:28 -0700

    starts bill-division.js

---
+++ HackerRank/bill-division.js
@@ -0,0 +1,11 @@
+function bonAppetit(bill, k, b) {
+  if ((bill.reduce((x, y) => x + y, 0) - bill[k]) / 2 === b) {
+    return "Bon Appetit";
+  } else {
+    return b - (bill.reduce((x, y) => x + y, 0) - bill[k]) / 2;
+  }
+}
+console.log(bonAppetit([3, 10, 2, 9], 1, 12));
+
+// should be Bon Appetit
+console.log(bonAppetit([3, 10, 2, 9], 1, 7));
